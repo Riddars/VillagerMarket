6d9ddd2fa816b0bdd26c37bbe5250c969288a882
Arsen Sarkisyan <80139269+Riddars@users.noreply.github.com>
2025-09-14 16:36:39 +0300

    Загружен обновлённый файл меню хранилища
    с обновлённой логикой переключения первой/последней страницы

---
+++ src/main/java/net/bestemor/villagermarket/menu/StorageMenu.java
@@ -0,0 +1,208 @@
+package net.bestemor.villagermarket.menu;
+
+import net.bestemor.core.config.ConfigManager;
+import net.bestemor.core.menu.Clickable;
+import net.bestemor.core.menu.Menu;
+import net.bestemor.core.menu.MenuContent;
+import net.bestemor.villagermarket.utils.VMUtils;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class StorageMenu extends Menu {
+
+    private final StorageHolder holder;
+
+    private final boolean isInfinite;
+    private final int inventorySize;
+    private final int itemsSize;
+    private final int page;
+
+    private List<ItemStack> items = new ArrayList<>();
+
+    public StorageMenu(StorageHolder holder, int size, int page) {
+        super(size == 0 ? 54 : size, ConfigManager.getString("menus.storage.title") + (size == 0 ? " | " + (page + 1) : ""));
+        this.holder = holder;
+
+        this.page = page;
+        this.isInfinite = size == 0;
+        this.inventorySize = size == 0 ? 54 : size;
+        this.itemsSize = isInfinite ? 45 : inventorySize - 1;
+    }
+
+    @Override
+    protected void onCreate(MenuContent content) {
+        getInventory().setContents(items.toArray(new ItemStack[0]));
+        update();
+    }
+
+
+    @Override
+    protected void onUpdate(MenuContent content) {
+        if (isInfinite) {
+            content.fillBottom(ConfigManager.getItem("items.filler").build());
+
+            int lastPage = holder.getPages() - 1;
+
+            // Кнопка "НА ПЕРВУЮ СТРАНИЦУ"
+            if (page > 0) { // Показываем, если мы не на первой странице
+                content.setClickable(47, Clickable.of(ConfigManager.getItem("items.first_page").build(), event -> {
+                    Player player = (Player) event.getWhoClicked();
+                    player.playSound(player.getLocation(), ConfigManager.getSound("sounds.menu_click"), 0.5f, 1);
+                    holder.open(player, 0); // Открываем страницу 0
+                }));
+            }
+
+            // Кнопка "НАЗАД" (предыдущая страница)
+            if (page > 0) {
+                content.setClickable(48, Clickable.of(ConfigManager.getItem("items.previous").build(), event -> {
+                    Player player = (Player) event.getWhoClicked();
+                    player.playSound(player.getLocation(), ConfigManager.getSound("sounds.menu_click"), 0.5f, 1);
+                    holder.open(player, page - 1);
+                }));
+            }
+
+            // Кнопка "ВПЕРЕД" (следующая страница)
+            if (page < lastPage) {
+                content.setClickable(50, Clickable.of(ConfigManager.getItem("items.next").build(), event -> {
+                    Player player = (Player) event.getWhoClicked();
+                    player.playSound(player.getLocation(), ConfigManager.getSound("sounds.menu_click"), 0.5f, 1);
+                    holder.open(player, page + 1);
+                }));
+            }
+
+            // Кнопка "НА ПОСЛЕДНЮЮ СТРАНИЦУ"
+            if (page < lastPage) { // Показываем, если мы не на последней странице
+                content.setClickable(51, Clickable.of(ConfigManager.getItem("items.last_page").build(), event -> {
+                    Player player = (Player) event.getWhoClicked();
+                    player.playSound(player.getLocation(), ConfigManager.getSound("sounds.menu_click"), 0.5f, 1);
+                    holder.open(player, lastPage); // Открываем последнюю страницу
+                }));
+            }
+
+        }
+        content.setClickable(inventorySize - 1, Clickable.of(ConfigManager.getItem("items.back").build(), event -> {
+            Player player = (Player) event.getWhoClicked();
+            this.holder.back(player);
+            player.playSound(player.getLocation(), ConfigManager.getSound("sounds.back"), 0.5f, 1);
+        }));
+    }
+
+    @Override
+    protected void onClick(InventoryClickEvent event) {
+        int size = event.getView().getTopInventory().getSize();
+        if (!holder.isAddAllowed()) {
+            if (event.getRawSlot() >= size && event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY) {
+                event.setCancelled(true);
+                return;
+            }
+            if (event.getRawSlot() < size && event.getCursor() != null && event.getCursor().getType() != Material.AIR) {
+                event.setCancelled(true);
+                return;
+            }
+        }
+        if (isInfinite ? (event.getRawSlot() < size - 9 || event.getRawSlot() > size - 1) : (event.getRawSlot() != size - 1)) {
+            event.setCancelled(false);
+        }
+        holder.onClick();
+    }
+
+    @Override
+    protected void onDrag(InventoryDragEvent event) {
+        holder.onClick();
+    }
+
+    public void setItems(List<ItemStack> items) {
+        int start = Math.min(items.size(), page * itemsSize);
+        int end = Math.min(items.size(), (page + 1) * itemsSize);
+        this.items = items.subList(start, end);
+        create();
+    }
+
+    public int getAmount(ItemStack i) {
+        int sum = 0;
+        for (int slot = 0; slot < itemsSize && slot < getInventory().getSize(); slot++) {
+            ItemStack storageItem = getInventory().getItem(slot);
+            if (VMUtils.compareItems(storageItem, i)) {
+                sum += storageItem.getAmount();
+            }
+        }
+        return sum;
+    }
+
+    public int getAvailableSpace(ItemStack i) {
+        int inStorage = getAmount(i);
+        int availableSlots = 0;
+        if (getInventory() == null) {
+            return 0;
+        }
+        for (int slot = 0; slot < itemsSize && slot < getInventory().getSize(); slot++) {
+            ItemStack storageItem = getInventory().getItem(slot);
+            if (storageItem == null || VMUtils.compareItems(storageItem, i)) {
+                availableSlots++;
+            }
+        }
+
+        return availableSlots * i.getType().getMaxStackSize() - inStorage;
+    }
+
+    public boolean isEmpty() {
+        return isInfinite && Arrays.stream(getInventory().getContents()).filter(Objects::isNull).count() > 44;
+    }
+
+    public List<ItemStack> getItems() {
+        List<ItemStack> items = new ArrayList<>();
+        for (int slot = 0; slot < itemsSize && slot < getInventory().getSize(); slot++) {
+            items.add(getInventory().getItem(slot));
+        }
+        items.removeIf(Objects::isNull);
+        return items;
+    }
+
+    public void addItem(ItemStack item, int amount) {
+        int stacks = amount / item.getType().getMaxStackSize();
+        int remainder = amount % item.getType().getMaxStackSize();
+        for (int i = 0; i < stacks; i++) {
+            ItemStack clone = item.clone();
+            clone.setAmount(item.getType().getMaxStackSize());
+            getInventory().addItem(clone);
+        }
+        if (remainder > 0) {
+            ItemStack clone = item.clone();
+            clone.setAmount(remainder);
+            getInventory().addItem(clone);
+        }
+    }
+
+    public int removeItem(ItemStack i, int amount) {
+        int removed = 0;
+        for (int slot = 0; slot < getInventory().getContents().length; slot++) {
+            ItemStack storageItem = getInventory().getContents()[slot];
+
+            if (storageItem == null) {
+                continue;
+            }
+            if (!VMUtils.compareItems(i, storageItem)) {
+                continue;
+            }
+            if (amount - removed >= storageItem.getAmount()) {
+                getInventory().setItem(slot, null);
+                removed += storageItem.getAmount();
+            } else {
+                storageItem.setAmount(storageItem.getAmount() - (amount - removed));
+                removed += (amount - removed);
+                break;
+            }
+        }
+        return removed;
+    }
+}
